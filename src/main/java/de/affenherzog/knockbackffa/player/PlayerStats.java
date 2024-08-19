9773fee6072cb0e970e51e2e8872c5e06fdab314
Julius <julius.rothgaenger@gmail.com>
2024-08-19 20:20:26 +0200

    change record to class, add a first join static stats-object with config values

--- src/main/java/de/affenherzog/knockbackffa/player/PlayerStats.java
+++ src/main/java/de/affenherzog/knockbackffa/player/PlayerStats.java
@@ -1,5 +1,34 @@
 package de.affenherzog.knockbackffa.player;
 
-public record PlayerStats(int kills, int deaths, int money, int rankPoints) {
+import de.affenherzog.knockbackffa.Kffa;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+public class PlayerStats {
+
+  public static final PlayerStats FIRST_JOIN_PLAYER_STATS = fetchFirstJoinPlayerStats();
+
+  @Getter @Setter
+  private int kills, deaths, money, rankPoints;
+
+  private static @NotNull PlayerStats fetchFirstJoinPlayerStats() {
+    final FileConfiguration config = Kffa.getInstance().getConfig();
+    final String path = "player-first-join-values.";
+
+    return new PlayerStats(
+        config.getInt(path + "kills"),
+        config.getInt(path + "deaths"),
+        config.getInt(path + "money"),
+        config.getInt(path + "rankPoints"));
+  }
+
+  public PlayerStats(int kills, int deaths, int money, int rankPoints) {
+    this.kills = kills;
+    this.deaths = deaths;
+    this.money = money;
+    this.rankPoints = rankPoints;
+  }
 
 }
