a92f326989cf9eda7310f6b8e1cd7ae2810265bc
Julius <julius.rothgaenger@gmail.com>
2024-08-19 21:18:27 +0200

    add initPlayer to set some player values

--- src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
+++ src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
@@ -3,6 +3,7 @@ package de.affenherzog.knockbackffa.player;
 import de.affenherzog.knockbackffa.Kffa;
 import de.affenherzog.knockbackffa.game.Game;
 import lombok.Getter;
+import org.bukkit.WeatherType;
 import org.bukkit.entity.Player;
 
 public class KffaPlayer {
@@ -16,6 +17,15 @@ public class KffaPlayer {
     this.player = player;
     this.playerStats = playerStats;
     this.game = Kffa.getInstance().getGame();
+
+    initPlayer();
+  }
+
+  private void initPlayer() {
+    player.setHealthScaled(true);
+    player.setHealthScale(6);
+    player.setSaturation(20);
+    player.setPlayerWeather(WeatherType.CLEAR);
   }
 
 }
