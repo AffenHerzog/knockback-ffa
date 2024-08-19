3547c59070e59e583df5dc6a0fe455e3cd846e67
Julius <julius.rothgaenger@gmail.com>
2024-08-19 20:21:59 +0200

    change constructor parameter, remove fetchPlayerStats method

--- src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
+++ src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
@@ -1,24 +1,21 @@
 package de.affenherzog.knockbackffa.player;
 
+import de.affenherzog.knockbackffa.Kffa;
 import de.affenherzog.knockbackffa.game.Game;
+import lombok.Getter;
 import org.bukkit.entity.Player;
 
 public class KffaPlayer {
   final Player player;
   final Game game;
 
-  final PlayerStats playerStats;
+  @Getter
+  private final PlayerStats playerStats;
 
-  public KffaPlayer(Player player, Game game) {
+  public KffaPlayer(Player player, PlayerStats playerStats) {
     this.player = player;
-    this.game = game;
-    this.playerStats = fetchPlayerStats();
+    this.playerStats = playerStats;
+    this.game = Kffa.getInstance().getGame();
   }
-
-  private PlayerStats fetchPlayerStats() {
-    //TODO ADD DB
-    return null;
-  }
-
 
 }
