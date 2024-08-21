b22f5115dc9ca5b40b5441033f333b4ff40f29db
Julius <julius.rothgaenger@gmail.com>
2024-08-21 20:44:36 +0200

    create LastHitTracker to store the player who hit last

--- src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
+++ src/main/java/de/affenherzog/knockbackffa/player/KffaPlayer.java
@@ -2,21 +2,29 @@ package de.affenherzog.knockbackffa.player;
 
 import de.affenherzog.knockbackffa.Kffa;
 import de.affenherzog.knockbackffa.game.Game;
+import de.affenherzog.knockbackffa.util.LastHitTracker;
 import lombok.Getter;
 import org.bukkit.WeatherType;
 import org.bukkit.entity.Player;
 
 public class KffaPlayer {
+
+  @Getter
   final Player player;
   final Game game;
 
   @Getter
   private final PlayerStats playerStats;
 
+  @Getter
+  private final LastHitTracker lastHitTracker;
+
   public KffaPlayer(Player player, PlayerStats playerStats) {
     this.player = player;
     this.playerStats = playerStats;
     this.game = Kffa.getInstance().getGame();
+
+    this.lastHitTracker = new LastHitTracker();
 
     initPlayer();
   }
---
+++ src/main/java/de/affenherzog/knockbackffa/util/LastHitTracker.java
@@ -0,0 +1,12 @@
+package de.affenherzog.knockbackffa.util;
+
+import de.affenherzog.knockbackffa.player.KffaPlayer;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class LastHitTracker {
+  private KffaPlayer lastInteract;
+
+}
