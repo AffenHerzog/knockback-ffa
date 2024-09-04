c02f2177cc3c53f085f2e393a1b4c86d0fd6c290
Julius <julius.rothgaenger@gmail.com>
2024-09-04 15:39:14 +0200

    add checkInFightLeave

--- src/main/java/de/affenherzog/knockbackffa/listener/PlayerQuitListener.java
+++ src/main/java/de/affenherzog/knockbackffa/listener/PlayerQuitListener.java
@@ -1,6 +1,7 @@
 package de.affenherzog.knockbackffa.listener;
 
 import de.affenherzog.knockbackffa.Kffa;
+import de.affenherzog.knockbackffa.player.DeathHandler;
 import de.affenherzog.knockbackffa.player.KffaPlayer;
 import de.affenherzog.knockbackffa.player.PlayerRepositoryImpl;
 import org.bukkit.entity.Player;
@@ -17,10 +18,17 @@ public class PlayerQuitListener implements Listener {
     final KffaPlayer kffaPlayer = Kffa.getInstance().getPlayerHashMap().get(player);
 
     if (kffaPlayer != null) {
+      checkInFightLeave(kffaPlayer);
       PlayerRepositoryImpl.getINSTANCE().update(kffaPlayer);
       Kffa.getInstance().getPlayerHashMap().remove(player);
     }
 
+  }
+
+  private void checkInFightLeave(@NotNull KffaPlayer kffaPlayer) {
+    if (kffaPlayer.getInFightTracker().isInFight()) {
+      DeathHandler.handleDeath(kffaPlayer.getPlayer());
+    }
   }
 
 }
