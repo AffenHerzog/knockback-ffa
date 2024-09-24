ddb3756f8bf5bc1a4d65df34acf27d6094d0068a
Julius <julius.rothgaenger@gmail.com>
2024-09-24 21:53:06 +0200

    add the one and only upgrade for the default kit, change ep kit and stick color

--- src/main/java/de/affenherzog/knockbackffa/player/kit/upgrade/DefaultKitUpgrade.java
+++ src/main/java/de/affenherzog/knockbackffa/player/kit/upgrade/DefaultKitUpgrade.java
@@ -1,10 +1,19 @@
 package de.affenherzog.knockbackffa.player.kit.upgrade;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class DefaultKitUpgrade extends KitUpgrade {
 
   public static final String FIELD_TYPE = "DefaultKitUpgrade";
 
-  public DefaultKitUpgrade(int price, int doubleJumpCooldown, int stickKnockback) {
+  @JsonCreator
+  public DefaultKitUpgrade(
+      @JsonProperty("price") int price,
+      @JsonProperty("doubleJumpCooldown") int doubleJumpCooldown,
+      @JsonProperty("stickKnockback") int stickKnockback
+  ) {
+
     super(price, doubleJumpCooldown, stickKnockback);
   }
 }
